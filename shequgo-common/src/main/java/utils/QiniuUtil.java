908ed824feacb15a9f5bf058d3789452090656ed
ColinLiugz <1368338445@qq.com>
2019-04-19 19:37:01 +0800

    test

--- shequgo-common/src/main/java/utils/QiniuUtil.java
+++ shequgo-common/src/main/java/utils/QiniuUtil.java
@@ -7,33 +7,13 @@ import com.qiniu.util.Auth;
  * @Date: 2019/3/18 23:15
  */
 public class QiniuUtil {
-    private static final String ACCESSKEY="";
-    private static final String SECRETKRY="";
+    private static final String ACCESSKEY="w7xWUYVujvAkBom1UFFJF4zOe8_84nGpYvw1kT0e";
+    private static final String SECRETKRY="gKvq53onLqpaWwrtyAvFzY_PTzeylDednfPCe-2J";
     private static final String BUCKET ="shequgo";
 
-    public String getTocken(){
+    public static String getTocken(){
         Auth auth = Auth.create(ACCESSKEY,SECRETKRY);
         String tocken = auth.uploadToken(BUCKET);
         return tocken;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 }
---
+++ shequgo-web/src/main/java/com/shequgo/shequgoweb/controller/CommonController.java
@@ -0,0 +1,37 @@
+package com.shequgo.shequgoweb.controller;
+
+import base.Admin;
+import com.shequgo.shequgoweb.filter.UserUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import utils.ApiResult;
+import utils.QiniuUtil;
+
+/**
+ * @Author: Colin
+ * @Date: 2019/4/19 19:29
+ */
+@Api(value = "CommonController",description = "公共接口")
+@RestController
+@CrossOrigin
+@RequestMapping(value = "/web")
+public class CommonController {
+
+
+    @ApiOperation(value = "获得七牛云token")
+    @RequestMapping(value = "/get/qiniuToken", method = RequestMethod.POST)
+    public ApiResult getQiniuToken(){
+        Admin admin ;
+        try {
+            admin = UserUtil.getCurrentUser();
+        } catch (Exception e) {
+            return new ApiResult(401,"未登录");
+        }
+        return ApiResult.ok(QiniuUtil.getTocken());
+    }
+
+}
